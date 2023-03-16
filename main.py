b5dacbfe3a3582b71d308592b85b31823a6e6ede
Tahmid Al Hannan <86019872+Ahiyawesome@users.noreply.github.com>
2023-03-15 18:26:55 -0700

    Completed the edited copy
    Completed the copy from the demo file; exported the poses to the video file 'SwordFight3.mp4'.

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from mmpose.apis import inference_pose_lifter_model, init_pose_model, process_mm
 from mmdet.apis import inference_detector, init_detector
 from mmpose.core import Smoother
 from mmpose.datasets import DatasetInfo
+from demo.body3d_two_stage_video_demo import convert_keypoint_definition
+import os.path as osp
 import mmcv
+import cv2
 import copy
 import pickle
 
@@ -12,13 +15,12 @@ import pickle
 
 def main():
     vid = mmcv.VideoReader('SwordFight.mp4')
-    # cfg = mmcv.Config.fromfile(
-    #     'mmpose/configs/body/3d_kpt_sview_rgb_vid/video_pose_lift/h36m/videopose3d_h36m_27frames_fullconv_supervised.py')
-    # pretrained_model = 'model/videopose_h36m_27frames_fullconv_supervised-fe8fbba9_20210527.pth'
     cfg = mmcv.Config.fromfile('mmpose/configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/coco/mspn50_coco_256x192.py')
     pretrained_model = 'model/mspn50_coco_256x192-8fbfb5d0_20201123.pth'
     det_config = 'mmpose/demo/mmdetection_cfg/faster_rcnn_r50_fpn_coco.py'
     det_checkpoint = 'model/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
+    pose_lift_config = mmcv.Config.fromfile('mmpose/configs/body/3d_kpt_sview_rgb_vid/video_pose_lift/h36m/videopose3d_h36m_243frames_fullconv_supervised_cpn_ft.py')
+    pose_lift_checkpoint = 'model/videopose_h36m_243frames_fullconv_supervised_cpn_ft-88f5abbb_20210527.pth'
 
     # cfg.data_cfg.image_size = (256, 256)
     pose_model = init_pose_model(cfg, pretrained_model)
@@ -43,6 +45,7 @@ def main():
             new_extraction = input("pose detection results list detected, do you want a new one? [y/n]")
             if new_extraction == 'y':
                 need_extraction = True
+                pose_det_results_list = []
             else:
                 print("Continuing onto next stage with loaded data")
 
@@ -79,6 +82,89 @@ def main():
 
     print('Stage 2: 2D-to-3D pose lifting.')
     # TODO: Do second stage
+    pose_lift_model = init_pose_model(pose_lift_config, pose_lift_checkpoint)
+    pose_lift_dataset = pose_lift_model.cfg.data['test']['type']
+
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    fps = vid.fps
+    writer = None
+
+    for pose_det_results in pose_det_results_list:
+        for res in pose_det_results:
+            keypoints = res['keypoints']
+            res['keypoints'] = convert_keypoint_definition(
+                keypoints, pose_det_dataset, pose_lift_dataset)
+
+    if hasattr(pose_lift_model.cfg, 'test_data_cfg'):
+        data_cfg = pose_lift_model.cfg.test_data_cfg
+    else:
+        data_cfg = pose_lift_model.cfg.data_cfg
+
+    pose_lift_dataset_info = pose_lift_model.cfg.data['test'].get(
+        'dataset_info', None)
+
+    pose_lift_dataset_info = DatasetInfo(pose_lift_dataset_info)
+
+    num_instances = -1
+    print('Running 2D-to-3D pose lifting inference...')
+    for i, pose_det_results in enumerate(
+            mmcv.track_iter_progress(pose_det_results_list)):
+        # extract and pad input pose2d sequence
+        pose_results_2d = extract_pose_sequence(
+            pose_det_results_list,
+            frame_idx=i,
+            causal=data_cfg.causal,
+            seq_len=data_cfg.seq_len,
+            step=data_cfg.seq_frame_interval)
+
+        # 2D-to-3D pose lifting
+        pose_lift_results = inference_pose_lifter_model(
+            pose_lift_model,
+            pose_results_2d=pose_results_2d,
+            dataset=pose_lift_dataset,
+            dataset_info=pose_lift_dataset_info,
+            with_track_id=True,
+            image_size=vid.resolution)
+
+        # Pose processing
+        pose_lift_results_vis = []
+        for idx, res in enumerate(pose_lift_results):
+            keypoints_3d = res['keypoints_3d']
+            # exchange y,z-axis, and then reverse the direction of x,z-axis
+            keypoints_3d = keypoints_3d[..., [0, 2, 1]]
+            keypoints_3d[..., 0] = -keypoints_3d[..., 0]
+            keypoints_3d[..., 2] = -keypoints_3d[..., 2]
+            # rebase height (z-axis)
+            res['keypoints_3d'] = keypoints_3d
+            # add title
+            det_res = pose_det_results[idx]
+            instance_id = det_res['track_id']
+            res['title'] = f'Prediction ({instance_id})'
+            # only visualize the target frame
+            res['keypoints'] = det_res['keypoints']
+            res['bbox'] = det_res['bbox']
+            res['track_id'] = instance_id
+            pose_lift_results_vis.append(res)
+
+        # Visualization
+        if num_instances < 0:
+            num_instances = len(pose_lift_results_vis)
+        img_vis = vis_3d_pose_result(
+            pose_lift_model,
+            result=pose_lift_results_vis,
+            img=vid[i],
+            dataset=pose_lift_dataset,
+            dataset_info=pose_lift_dataset_info,
+            out_file=None)
+
+        if writer is None:
+            writer = cv2.VideoWriter(
+                "SwordFight3.mp4", fourcc,
+                fps, (img_vis.shape[1], img_vis.shape[0]))
+        writer.write(img_vis)
+
+    writer.release()
+    # TODO: Add Pickle for 3D poses, add video file to show all poses.
 
 
 if __name__ == '__main__':
