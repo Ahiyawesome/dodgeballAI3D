9169971f307c8cb2c64d96a9d048ed4b1118963c
Tahmid Al Hannan <86019872+Ahiyawesome@users.noreply.github.com>
2023-04-07 16:27:50 -0700

    Finished the animation implementation
    Added the rest of the code so that each bone in each frame is animated, and added a limitation on how much the bone can displace, so that the animation isn't too weird. On the Blender app, I have to get rid of the links between the bones because when I change a certain bone location, its children and parents also are changed.

--- extraction_implementation_blender.py
+++ extraction_implementation_blender.py
@@ -1,35 +1,53 @@
 import bpy
-import mathutils as mt
+from mathutils import Vector
+
+# Maximum Location Difference
+MLD = 0.02
+
 
 def main():
+    bpy.ops.object.mode_set(mode='POSE')
+    scene, object, bones = initialize()
     index = 0
+    cur_frame = 1
     with open(r"C:\Users\ahiya\BlenderStuff\Keypoints_3D_One.txt", "rt") as keypoints:
         for line in keypoints:
-            bone = get_bone_type(index)
+            bone = get_bone(index, bones)
             if len(line) == 51 or len(line) == 52:
                 type = 0
-    #            print(line[2:49])
             elif len(line) == 48 or len(line) == 49:
                 type = 1
-    #            print(line[2:46])
-    
-            """ Testing just the first frame """
             else:
-                break
-            
+                object.keyframe_insert_by_name(type='LocRotScale')
+                scene.frame_set(cur_frame)
+                cur_frame += 1
+                continue
+
             change_bone(type, line, bone)
             index += 1
-
-    #bone = bpy.data.objects['Armature'].data.edit_bones["Lower Arm.L"]
-    #bone_loc = mt.Vector((0, 0, 0))
-    #bone.head = bone_loc
+            if index == 16:
+                index = 0
 
     print("WORKED")
-    
-def get_bone_type(index):
-    pass
-    # TODO: Find out which keypoint corresponds to which body part, then update this code
-    
+
+
+def initialize():
+    object = bpy.ops.anim
+    bones = ["Hip", "Upper Leg.R", "Lower Leg.R", "Foot.R", "Upper Leg.L", "Lower Leg.L", "Foot.L", "Spine", "Chest",
+             "Neck", "Head", "Shoulder.L", "Upper Arm.L", "Lower Arm.L", "Shoulder.R", "Upper Arm.R", "Lower Arm.R"]
+    frame_rate = 30
+    scene = bpy.context.scene
+    scene.frame_set(1)
+    scene.render.fps = frame_rate
+    return scene, object, bones
+
+
+def get_bone(index, bones):
+    bone = bones[index]
+    bone = bpy.context.object.pose.bones[bone]
+    return bone
+
+
 def change_bone(type, values, bone):
     if type == 0:
         x = values[2:17]
@@ -39,6 +57,45 @@ def change_bone(type, values, bone):
         x = values[2:16]
         y = values[17:31]
         z = values[32:46]
+
+    """ If the value is positive, there's a space in front of the string """
+    if x[0] == " ":
+        x = x[1:]
+    if y[0] == " ":
+        y = y[1:]
+    if z[0] == " ":
+        z = z[1:]
+
+    x, y, z = location_modifier(bone, x, y, z)
+
+    bone.location = Vector((x, y, z))
+
+
+def location_modifier(bone, x, y, z):
+    x = float(x)
+    y = float(y)
+    z = float(z)
+
+    if abs(x - bone.location.x) > MLD:
+        if x > bone.location.x:
+            x = bone.location.x + MLD
+        else:
+            x = bone.location.x - MLD
+
+    if abs(y - bone.location.y) > MLD:
+        if y > bone.location.y:
+            y = bone.location.y + MLD
+        else:
+            y = bone.location.y - MLD
+
+    if abs(z - bone.location.z) > MLD:
+        if z > bone.location.z:
+            z = bone.location.z + MLD
+        else:
+            z = bone.location.z - MLD
+
+    return x, y, z
+
 
 if __name__ == "__main__":
     main()
